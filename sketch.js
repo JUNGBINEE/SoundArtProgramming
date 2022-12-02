cce69004cbe65cf27398eb6ed3e7be7683b16fbc
JUNGBINEE <102014914+JUNGBINEE@users.noreply.github.com>
2022-12-02 15:41:15 +0900

    가속도 센서

--- sketch.js
+++ sketch.js
@@ -1,7 +1,20 @@
-function setup() {
-  createCanvas(400, 400);
+//
+
+let r, g, b;
+function setup(){
+  createCanvas(displayWidth, displayHeight);
+  r = random(50, 255);
+  g = random(0, 200);
+  b = random(50, 255);
 }
 
-function draw() {
-  background(220);
+function draw(){
+  background(r, g, b);
+  console.log('draw');
+}
+
+function deviceMoved(){
+  r = map(accelerationX, -90, 90, 100, 175);
+  g = map(accelerationY, -90, 90, 100, 200);
+  b = map(accelerationZ, -90, 90, 100, 200); 
 }
